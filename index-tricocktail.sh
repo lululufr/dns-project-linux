343e6cdf3a3214f73f0f91bb1697589894e8cc60
lululufr <lucas.lm.miller@gmail.com>
2023-07-03 22:12:14 +0200

    dazdzadza

--- index-tricocktail.sh
+++ index-tricocktail.sh
@@ -1,30 +1,5 @@
 #!/bin/bash
 
-# Fonction pour afficher la taille formatée en Go, Mo, Ko et octets
-format_size() {
-    local size=$1
-    local unit="octets"
-
-    if [[ $size == *M ]]; then
-        size=${size%M}
-        unit="Mo"
-    fi
-
-    if ((size >= 1024)); then
-        size=$(bc <<< "scale=2; $size / 1024")
-        unit="Go"
-    fi
-
-    if ((size >= 1024)); then
-        size=$(bc <<< "scale=2; $size / 1024")
-        unit="To"
-    fi
-
-    printf "%.2f%s" "$size" "$unit"
-}
-
-
-
 
 # Calcul de la taille des répertoires personnels
 declare -A sizes
@@ -36,16 +11,8 @@ while IFS=: read -r username _; do
     fi
 done < <(cut -d: -f1,6 /etc/passwd)
 
-
-
 # Tri cocktail des utilisateurs en fonction de la taille
-
-
-tab=${sizes[@]}
-# Affichage du tableau
-
-echo "${sizes[@]}"
-
+tab=(${sizes[@]})
 
 echange=true
 while [ "$echange" = true ]
@@ -54,7 +21,10 @@ do
 
     for i in $(seq 0 $((${#tab[@]} - 2)))
     do
-        if [ ${tab[$i]} -lt ${tab[$((i+1))]} ]
+        value1=${tab[$i]//[!0-9]/}
+        value2=${tab[$((i+1))]//[!0-9]/}
+        
+        if [ $value1 -lt $value2 ]
         then
             tmp=${tab[$i]}
             tab[$i]=${tab[$((i+1))]}
@@ -62,10 +32,13 @@ do
             echange=true
         fi
     done
-
+    
     for i in $(seq $((${#tab[@]} - 2)) -1 0)
     do
-        if [ ${tab[$i]} -lt ${tab[$((i+1))]} ]
+        value1=${tab[$i]//[!0-9]/}
+        value2=${tab[$((i+1))]//[!0-9]/}
+        
+        if [ $value1 -lt $value2 ]
         then
             tmp=${tab[$i]}
             tab[$i]=${tab[$((i+1))]}
@@ -77,28 +50,28 @@ done
 
 echo "${tab[@]}"
 
+# Affichage des 5 plus gros consommateurs d'espace lors de la connexion
+echo "Les 5 plus gros consommateurs d'espace disque :"
 
-# Affichage des 5 plus gros
-echo "5 plus gros consommateurs d'espace:"
-
-for ((i = 0; i < 5 && i < ${#users[@]}; i++)); do
-    username=${users[$i]}
+for ((i = 0; i < 5 && i < ${#tab[@]}; i++)); do
+    username=${tab[$i]}
     size=${sizes[$username]}
-    formatted_size=$(format_size $size)
-    echo "$(($i + 1)). $username : $formatted_size"
+    formatted_size=($size)
+    echo "$(($i + 1)). $username"
+    #echo "$(find /home -size ${tab[i]})"
 done
 
 # Modification du fichier .bashrc de chaque utilisateur
-for username in "${users[@]}"; do
+for username in "${tab[@]}"; do
     bashrc_file="/home/$username/.bashrc"
 
     if [[ -f "$bashrc_file" ]]; then
         # Ajout de la commande pour afficher la taille du répertoire personnel
-        echo 'echo "Taille du repertoire  : $(du -sh ~ | cut -f1)"' >> "$bashrc_file"
-	# Verif taille
-        echo 'if (( $(du -sBM ~ | cut -f1) > 100 )); then' >> "$bashrc_file"
-        echo 'echo "AVERTISSEMENT : répertoire +  100 M" >&2' >> "$bashrc_file"
-        echo 'fi' >> "$bashrc_file"    
+        echo 'echo "Taille du répertoire personnel : $(du -sh ~ | cut -f1) ($(du -sB1 ~ | cut -f1) octets)"' >> "$bashrc_file"
 
-fi
+        # Vérification de la taille et ajout de l'avertissement si nécessaire
+        echo 'if (( $(du -sBM ~ | cut -f1) > 100 )); then' >> "$bashrc_file"
+        echo '    echo "AVERTISSEMENT : Votre répertoire personnel occupe plus de 100 Mo." >&2' >> "$bashrc_file"
+        echo 'fi' >> "$bashrc_file"
+    fi
 done

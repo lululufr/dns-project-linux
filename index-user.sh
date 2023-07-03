92ed7e78896faf2d0eef1c0f0d5ab4f9751a4966
lululufr <lucas.lm.miller@gmail.com>
2023-07-03 20:38:04 +0200

    scriptuser

--- index-user.sh
+++ index-user.sh
@@ -1,16 +1,43 @@
 #!/bin/bash
 
-noms=("Dufresn" "Morales" "Bathily" "Miller" "Elefteriou" "Bossboeuf" "Paris" "Sissoko" "Uzumaki" "Miyamoto" "Bienveillance")
-prenoms=("Désiré" "Alexis" "Delphine" "Backary" "Yvann" "Précieux" "Sublime" "Lucas" "Bob" "Quentin" "Perceval" "Cyrano" "Ping")
+if [ $# -ne 1 ]; then
+    echo "Veuillez renseigner le nom du fichier"
+    echo " la commande s'utilise comme suis : $0 <fichier.avec.les.utilisateurs>"
+    exit 1
+fi
+
+fichier_utilisateurs=$1
+
+if [ ! -f "$fichier_utilisateurs" ]; then
+    echo "$fichier_utilisateur n'est pas valide !!!!"
+    exit 1
+fi
 
 
-nom=${noms[$RANDOM % ${#noms[@]}]}
-prenom=${prenoms[$RANDOM % ${#prenoms[@]}]} #génération aléatoire d'un nom et d un prenom
+#lecture du fichier avec comme séparateur le : 
+#le tr permet de tout mettre toutes les majuscule en minuscule
+while IFS=':' read -r prenom nom groupes sudo password; do
+    username=$(echo "${prenom:0:1}${nom}" | tr '[:upper:]' '[:lower:]')
+    
+    if id -u "$username" >/dev/null 2>&1; then
+        username="${username}1"
+    fi
+    
+    useradd -p "$(openssl passwd -1 "$password")" -c "$prenom $nom" -m "$username"
+    chage -d 0 "$username"
+    
+    IFS=',' read -ra GROUP <<< "$groupes"
+    for i in "${GROUP[@]}"; do
+        if ! getent group "$i" >/dev/null 2>&1; then
+            groupadd "$i"
+        fi
+        usermod -a -G "$i" "$username"
+    done
+    
+    if [ "$sudo" = "oui" ]; then
+        usermod -aG sudo "$username"
+    fi
 
+done < "$fichier_utilisateurs"
 
-echo "Nom : $nom"
-echo "Prénom : $prenom"
-
-login="${prenom:0:1}${nom}"
-
-echo "$login"
+echo "Les utilisateurs de votre fichier ont été créés"
